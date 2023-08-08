89c7a73ae1e68f8ae242050427feea4ed5a4d936
Volodymyr Senyk <vova.senyk@gmail.com>
2023-08-08 17:57:23 +0300

    added check to tpl-parts

--- tpl-parts/top-panels/top-panel-default.php
+++ tpl-parts/top-panels/top-panel-default.php
@@ -1,9 +1,17 @@
 <?php
-if ( !empty( $args ) ) $part_args = $args;
+if ( !empty( $args ) ) {
+	$part_args = $args;
+	$container_class = $part_args['class'];
+	$title = $part_args['title'];
+} else {
+	$container_class = '';
+	$title = get_the_title( get_the_ID() );
+}
+$container_class = $container_class > 0 ? ' ' . $container_class : '';
 ?>
 
 <section class="top_panel top_panel__default">
-	<div class="container<?php echo $part_args['container_class'] ? ' ' . $part_args['container_class'] : ''; ?>">
-		<h1><?php echo $part_args['title'] ? : get_the_title( get_the_ID() ); ?></h1>
-	</div>
+    <div class="container<?php echo $container_class; ?>">
+        <h1><?php echo $title; ?></h1>
+    </div>
 </section>
