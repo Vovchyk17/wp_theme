8ff3883a45f65b74ed1a29162f680d7712c52afc
Volodymyr Senyk <vova.senyk@gmail.com>
2023-08-29 10:55:30 +0300

    add get_page_url function by tpl file

--- functions.php
+++ functions.php
@@ -49,3 +49,25 @@ function custom_tax_linked($pid, $tax, $template_slug) {
 		return $term_list;
 	}
 }
+
+//get page url by template name
+function get_page_url( $template_name ) {
+	$pages = get_posts( [
+		'post_type'   => 'page',
+		'post_status' => 'publish',
+		'meta_query'  => [
+			[
+				'key'     => '_wp_page_template',
+				'value'   => $template_name . '.php',
+				'compare' => '='
+			]
+		]
+	] );
+	if ( ! empty( $pages ) ) {
+		foreach ( $pages as $pages__value ) {
+			return get_permalink( $pages__value->ID );
+		}
+	}
+
+	return get_bloginfo( 'url' );
+}
