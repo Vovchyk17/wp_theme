1391596b782684f4091ce3571bef441a68275bc6
Volodymyr Senyk <vova.senyk@gmail.com>
2024-07-08 17:39:15 +0300

    block-gallery.php upd

--- tpl-parts/blocks/custom-gallery/block-gallery.php
+++ tpl-parts/blocks/custom-gallery/block-gallery.php
@@ -14,7 +14,7 @@ if ( $custom_gallery ) :
 	    <?php $g_i = 1; foreach ( $custom_gallery as $image ) : ?>
             <figure class="block__custom_gallery_item">
                 <a href="<?php echo esc_url( $image['url'] ); ?>" data-fancybox="custom-gallery" aria-label="Open image <?php echo $g_i++; ?>">
-                    <?php echo wp_get_attachment_image( $image['id'], 'custom_gallery', false, array( 'alt' => get_alt( $image['id'] ) ) ); ?>
+                    <?php echo wp_get_attachment_image( $image['id'], 'full', false, array( 'alt' => get_alt( $image['id'] ) ) ); ?>
                 </a>
             </figure>
 	    <?php endforeach; ?>
